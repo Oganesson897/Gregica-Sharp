c481a0ce93705bc4eb9811baec937f104be3008a
themorningglory233 <114865431+themorningglory233@users.noreply.github.com>
2023-08-29 22:34:55 +0800

    Update GSFirstDegreeMaterial.java

--- src/main/java/me/oganesson/gregicas/api/unification/materials/GSFirstDegreeMaterial.java
+++ src/main/java/me/oganesson/gregicas/api/unification/materials/GSFirstDegreeMaterial.java
@@ -23,7 +23,7 @@ public class GSFirstDegreeMaterial {
                 .blastTemp(9001, "HIGHER", 131072, 600)
                 .build();
 
-     NeutronsFlow = new Material.Builder(29201, gregtechId("neutrons_flow"))
+    NeutronsFlow = new Material.Builder(29201, gregtechId("neutrons_flow"))
                 .fluid(FluidTypes.GAS, false)
                 .color(16448250)
                 .build();
@@ -41,9 +41,26 @@ public class GSFirstDegreeMaterial {
                 .components(Materials.Bismuth, 47, Materials.Lead, 25, Materials.Tin, 13, Materials.Cadmium, 10, Materials.Indium, 5)
                 .build();
 
+    Inconel625 = new Material.Builder(29204, "inconel_625")
+                .ingot()
+                .fluid()
+                .components(Materials.Nickel, 3, Materials.Chromium, 7, Materials.Molybdenum, 10, Materials.Invar, 10, Materials.Nichrome, 13)
+                .flags(GENERATE_RING, GENERATE_BOLT_SCREW, GENERATE_GEAR)
+                .blastTemp(2700, "LOW", 120, 500)
+                .fluidTemp(2700)
+                .color(0x3fcc60)
+                .fluidPipeProperties(550, 6400, true, true, true, true)
+                .build();
 
-
-
+    MetallicHydrogen = new Material.Builder(29205, "metallic_hydrogen")
+                .ingot().fluid()
+                .iconSet(MaterialIconSet.SHINY)
+                .flags(GENERATE_PLATE, GENERATE_RING, GENERATE_ROUND, GENERATE_ROTOR, GENERATE_BOLT_SCREW, GENERATE_GEAR, GENERATE_SMALL_GEAR, GENERATE_LONG_ROD, GENERATE_FRAME)
+                .fluidTemp(1)
+                .color(0x4682B4)
+                .fluidPipeProperties(10240, 24000, true, true, true, true)
+                .components(Hydrogen, 1)
+                .build();
   
 }
 }
